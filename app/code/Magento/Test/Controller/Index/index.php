b50999af5d15c072bef40c1a8ff491fe7c7dc0a1
Yurii Yurakh <yurayurakh@gmail.com>
2018-05-19 20:20:18 +0300

    disable toolbar

---
+++ app/code/Magento/Test/Controller/Index/index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Magento\Test\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+
+    }
+}
---
+++ app/code/Magento/Test/Plugin/FilterColorDisable.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: george
+ * Date: 15.05.18
+ * Time: 19:27
+ */
+
+namespace Magento\Test\Plugin;
+
+use Magento\LayeredNavigation\Block\Navigation\FilterRendererInterface;
+
+class FilterColorDisable
+{
+
+//    public function beforeRender(FilterRendererInterface $subject, $result)
+//    {
+//
+//
+//        return $result;
+//    }
+}
